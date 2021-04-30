3d5e341c5ed2f5a9e76207f9a6267d8b2a1bc640
oeg2114 <60344248+oeg2114@users.noreply.github.com>
2021-04-30 19:52:58 +0200

    moved hiding function into external script shortened placement descriptions

---
+++ js/model-viewer-annotations.js
@@ -0,0 +1,13 @@
+﻿const modelViewer = document.querySelector('#logo-spaces');
+
+
+const checkbox = modelViewer.querySelector('#show-dimensions');
+checkbox.addEventListener('change', () => {
+    modelViewer.querySelectorAll('button').forEach((hotspot) => {
+        if (checkbox.checked) {
+            hotspot.classList.remove('hidden');
+        } else {
+            hotspot.classList.add('hidden');
+        }
+    });
+});
